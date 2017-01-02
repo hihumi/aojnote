061437e8ae792b3a38db12ac1e8974fb1fd4d554
hihumi <yui.maa5800@gmail.com>
2017-01-02 14:14:49 +0900

    add file

---
+++ itp1/sorting_three_numbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def sorting_three_numbers():
+    num_list = list(map(int, input().split()))
+
+    res = sorted(num_list)
+    print('{0} {1} {2}'.format(res[0], res[1], res[2]))
+
+    # Presentation Error:
+    #for i in res:
+    #   print('{} '.format(i), end = '')
+        #print(i, '', end = '')
+    #print()
+    # for i in res:
+    #    print(i, '', end = '')
+    #
+    #[print(i, '') for i in res]
+    #[print('{} '.format(i), end = '') for i in res]
+    #print()
+
+sorting_three_numbers()
