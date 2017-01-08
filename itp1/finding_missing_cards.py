e9051c743b97a355b0c5cf62b7968b1a512f4f53
hihumi <yui.maa5800@gmail.com>
2017-01-09 04:27:25 +0900

    add file

---
+++ itp1/finding_missing_cards.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def finding_missing_cards():
+    deck_l = [
+        '{0} {1}'.format(suit, rank)
+        for suit in ('S', 'H', 'C', 'D')
+        for rank in range(1, 14)
+    ]
+
+    n = int(input())
+    for _ in range(n):
+        S_H_C_D_and_rank = input()
+        deck_l.remove(S_H_C_D_and_rank)
+
+    for x in deck_l:
+        print(x)
+
+if __name__ == '__main__':
+    finding_missing_cards()
---
+++ itp1/official_house.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def official_house():
+    data = [[[0 for r in range(10)] for f in range(3)] for b in range(4)]
+
+    n = int(input())
+    for _ in range(n):
+        b, f, r, v = map(int, input().split())
+        data[b - 1][f - 1][r - 1] += v
+
+    for b in range(4):
+        for f in range(3):
+            for r in range(10):
+                # Presentation Error:
+                # print(' {} '.format(data[b][f][r]), end='')
+                print('', data[b][f][r], end = '')
+            print()
+        if b < 3:
+            print('#' * 20)
+
+if __name__ == '__main__':
+    official_house()
