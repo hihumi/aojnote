d5673c8ea19434ecbb37dcc819b3e4f1e6430ccc
hihumi <yui.maa5800@gmail.com>
2017-01-15 07:02:33 +0900

    add file

---
+++ volume0/digit_number.py
@@ -0,0 +1,12 @@
+#!/usr/bin_env python3
+
+import sys
+
+def digit_number():
+    line = sys.stdin.readlines()
+    for i in line:
+        a, b = map(int, i.split())
+        print(len(str(a + b)))
+
+if __name__ == '__main__':
+    digit_number()
