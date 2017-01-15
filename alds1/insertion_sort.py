a3c5613d221d5510b00038c7b4678082fa36ce94
hihumi <yui.maa5800@gmail.com>
2017-01-15 10:37:12 +0900

    make dir and file

---
+++ alds1/insertion_sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def insertion_sort(A, N):
+    for i in range(1, N):
+        v = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > v:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = v
+        
+        print(' '.join(map(str, A)))
+
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+    print(' '.join(map(str, A)))
+    insertion_sort(A, N)
