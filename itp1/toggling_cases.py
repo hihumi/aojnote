9d2d4640e84e0f00242a27c4728d221ef8ed6dab
hihumi <yui.maa5800@gmail.com>
2017-01-21 13:47:55 +0900

    add file

---
+++ itp1/toggling_cases.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def toggling_cases():
+    print(input().swapcase())
+
+    #
+    # or
+    # x = input().swapcase()
+    # print(x)
+    #
+
+if __name__ == '__main__':
+    toggling_cases()
